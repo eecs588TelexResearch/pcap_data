e047e97549afb99852382cf9fd9515a04e84b083
Nick Ruff <nickruff@umich.edu>
2014-04-12 23:47:13 -0400

    BUild dictionary

--- ml/datagen.py
+++ ml/datagen.py
@@ -9,37 +9,39 @@ from __future__ import print_function
 import dpkt
 import socket
 import sys
-import whois
+import pprint
+#import whois
 
-def dnsLookup(dst_ip):
-	try:
-		# tuple = socket.gethostbyaddr(dst_ip)
-		domain = whois.query(dst_ip)
-		print(domain.__dict__)
-		return domain.name
-	except socket.herror:
-		return "ERROR"
+# def dnsLookup(dst_ip):
+# 	try:
+# 		# tuple = socket.gethostbyaddr(dst_ip)
+# 		domain = whois.query(dst_ip)
+# 		print(domain.__dict__)
+# 		return domain.name
+# 	except socket.herror:
+# 		return "ERROR"
 
-def isBlocked(dst_ip):
-	return dst_ip.find('74.125') >= 0 or dst_ip.find('173.194') >= 0
-	# print(name)
-	# #return False
-	# # return name == 'ord08s08-in-f16.1e100.net'
-	# if name == "ERROR" :
-	# 	return False
-	# return name.find('1e100.net') > 0
+# def isBlocked(dst_ip):
+# 	return dst_ip.find('74.125') >= 0 or dst_ip.find('173.194') >= 0
+# 	# print(name)
+# 	# #return False
+# 	# # return name == 'ord08s08-in-f16.1e100.net'
+# 	# if name == "ERROR" :
+# 	# 	return False
+# 	# return name.find('1e100.net') > 0
 
 
 def main():
 	filename = sys.argv[1]
 	last_time = 0
+	flows = {}
 	for ts, pkt in dpkt.pcap.Reader(open(filename, 'r')):
-		#counter += 1
+		
 		eth = dpkt.ethernet.Ethernet(pkt)
 		if eth.type != dpkt.ethernet.ETH_TYPE_IP:
 			continue
 		ip = eth.data
-	    #ipcounter += 1
+	    
 		dst_ip_addr_str = socket.inet_ntoa(ip.dst)
 		if last_time == 0:
 			delta = 0
@@ -48,9 +50,30 @@ def main():
 
 		last_time = ts
 
+		packetList = [ ts, len(pkt), delta, dst_ip_addr_str ]
+
 		# size, time delta, is dest IP blocked
-		if dst_ip_addr_str.find("192.168") < 0:
-			print( len(pkt),",",delta,",",isBlocked(dst_ip_addr_str), sep='')
+		#if dst_ip_addr_str.find("192.168") < 0:
+		#	print( len(pkt),",",delta,",",isBlocked(dst_ip_addr_str), sep='')
+
+		# size, IAT, dest IP addr
+		#print( len(pkt),",",delta,",",dst_ip_addr_str, sep='')
+
+		ipkey = dst_ip_addr_str
+		home = '192.168.1.1'
+		###src_ip_addr_str = socket.inet_ntoa(ip.src)
+
+		if ipkey == home:
+			###ipkey = src_ip_addr_str
+			continue
+
+		if ipkey in flows:
+			flows[ipkey].append(packetList)
+		else:
+			flows[ipkey] = [ packetList ]
+
+	pprint.pprint(flows)
+
 
 if __name__ == "__main__":
     main()
